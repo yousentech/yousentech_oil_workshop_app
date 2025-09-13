a8baed7f3c055088d42fcaf1fc990d7293c46e9c
yousentech <83219678+yousentech@users.noreply.github.com>
2025-09-14 02:28:10 +0300

    UDATE

--- __manifest__.py
+++ __manifest__.py
@@ -5,14 +5,18 @@
     "depends": ["base", "account", "yousentech_oil_workshop"],
     "application": True,
     "version": "17.0.0.3",
-    "license": "AGPL-3",
-    "installable": True,
-    "data": [
+      "data": [
        'security/ir.model.access.csv',
         'security/security.xml',
         "view/services.xml",
         "view/service_type.xml",
         "view/package_app.xml",
     ],
-    "images": [],
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'sequence': '-100',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
 }
