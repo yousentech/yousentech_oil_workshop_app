1850fd2f7bf774afe8cc04ca9492ca6f2ef8bfa4
Mohammed Bamalliss <you@gmail.com>
2025-09-14 00:04:48 +0300

    update

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "yousentech_oil_workshop_app",
+    "description": "Additional Fields for YUVO Customer",
+    "author": "yousen tech Techno Solutions, Odoo SA",
+    "depends": ["base", "account", "yousentech_oil_workshop"],
+    "application": True,
+    "version": "17.0.0.3",
+    "license": "AGPL-3",
+    "installable": True,
+    "data": [
+        # "invoice_report/11_inv_normal.xml" ,
+        # "invoice_report/fency_report_invoic.xml",
+        # "view/account_move.xml",
+    ],
+    "images": [],
+}
---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+
+from . import account_move
+from . import res_company
+
+
