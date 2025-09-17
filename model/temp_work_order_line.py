d522df4a2a0ed288dff02f7a30c28851c5e84997
yousentech <83219678+yousentech@users.noreply.github.com>
2025-09-17 16:06:28 +0300

    udaes

--- model/temp_work_order_line.py
+++ model/temp_work_order_line.py
@@ -163,5 +163,5 @@ class work_order_package_detail_line(models.Model):
     _name = 'oil.wo.package.detail.line'
     
     service_id = fields.Many2one('oil.services',string="Service",)
-    product_id = fields.Many2one('product.product', string='Product', domain="[('detailed_type', 'in', ['service','product'])]", help="Select a service-type product", required=True)
+    product_id = fields.Many2one('product.product', string='Product', domain="[('detailed_type', 'in', ['service','product'])]", help="Select a service-type product",)
     header_id = fields.Many2one("oil.wo.package.detail",  ondelete="cascade",copy=False)
