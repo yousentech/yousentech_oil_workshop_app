ca228ae74b66c9c27d674dcb3a2ef86a794fa00c
yousentech <83219678+yousentech@users.noreply.github.com>
2025-09-18 12:18:05 +0300

    Update account_move.py

--- model/account_move.py
+++ model/account_move.py
@@ -13,8 +13,9 @@ class account_move(models.Model):
         if res.work_order_app_id and res.move_type == 'out_invoice':
             res.work_order_app_id.write({'account_move_id': res.id})
             
-        is_exsiting_field = self.env['ir.model.fields'].sudo().search([('name', '=', 'salesman_id'), ('model', '=', 'oil.work.order')])
+        is_exsiting_field = self.env['ir.model.fields'].sudo().search([('name', '=', 'salesman_id'), ('model', '=', 'oil.work.order.app')])
         if is_exsiting_field:
           res.salesman_id = res.work_order_app_id.salesman_id.id
         return res
+
         
